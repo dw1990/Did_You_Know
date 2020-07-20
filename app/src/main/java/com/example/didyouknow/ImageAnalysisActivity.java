4ede8d98cad60006c6b23a0d5875332f7bfb9fed
dw1990 <denisweber90@googlemail.com>
2020-07-20 12:23:40 +0200

    Empty Activities added

---
+++ app/src/main/java/com/example/didyouknow/ImageAnalysisActivity.java
@@ -0,0 +1,14 @@
+package com.example.didyouknow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_analysis);
+    }
+}
