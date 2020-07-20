9da66daa41adba4af5e296a8d5ca6ccf24ef1543
dw1990 <denisweber90@googlemail.com>
2020-07-20 13:11:59 +0200

    added swipe detection on start activity and buttons

--- app/src/main/java/com/example/didyouknow/MainActivity.java
+++ app/src/main/java/com/example/didyouknow/MainActivity.java
@@ -1,56 +1,57 @@
 package com.example.didyouknow;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.didyouknow.utility.OnSwipeTouchListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MotionEvent;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.swipe_container).setOnTouchListener(new OnSwipeTouchListener(this){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSwipeRight() {
+                navigateToCameraActivity();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                navigateToFavoritesActivity();
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+    public void navigateToCamera(View view){
+        this.navigateToCameraActivity();
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void navigateToFavoritesActivity (View view){
+        this.navigateToFavoritesActivity();
+    }
+
+    private void navigateToCameraActivity(){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    private void navigateToFavoritesActivity(){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 }
